ca935dff896f80fa4d2ef47fd95f729477e32b3f
Hamza Ahmad <hamzaahmad8644@gmail.com>
2021-10-05 01:45:08 +0500

    1st section css complete, 2nd section markup complete

--- src/Header.js
+++ src/Header.js
@@ -2,44 +2,47 @@ import React from "react";
 
 const Header = () => {
   return (
-    <header>
-      <section className="container main-hero-container">
-        <div className="row">
-          <div className="col-12 col-lg-6 header-left-side d-flex justify-content-center flex-column align-item-start">
-            <h1 className="display-2">
-              Online Payment Made <br /> Easy For YOu.
-            </h1>
-            <p className="main-hero-para">
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laborum
-              libero praesentium ducimus, voluptatibus alias tempore aspernatur
-              at dolorum odio totam facilis animi corporis suscipit rerum
-              voluptatum maxime pariatur eveniet quisquam?
-            </p>
-            <h3>Get early access for you</h3>
-            <div className="input-group mt-3">
-              <input
-                type="text"
-                className="rounded-pill w-75 me3 p-2 form-control-text"
+    <>
+      <header>
+        <section className="container main-hero-container">
+          <div className="row">
+            <div className="col-12 col-lg-6 header-left-side d-flex justify-content-center flex-column align-items-start ">
+              <h1 className="display-2">
+                Online Payment Made <br /> Easy For You.
+              </h1>
+              <p className="main-hero-para">
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et,
+                quia quis? Ipsa officia ad deserunt voluptate quam, nisi odio
+                officiis tempora recusandae voluptate quam, nisi odio officiis
+                tempora recusandae
+              </p>
+              <h3>Get early access for you</h3>
+              <div className="input-group mt-3">
+                <input
+                  type="text"
+                  className="rounded-pill w-50  w-lg-75 me-3 p-2 form-control-text"
+                  placeholder="Enter Your Email"
+                />
+                <div className="input-group-button">Get it now</div>
+              </div>
+            </div>
+            {/*  --------------- main header right side--------------  */}
+            <div className="col-12 col-lg-6 header-right-side d-flex justify-content-center align-items-center main-herosection-images">
+              <img
+                src="./images/hero1.jpg"
+                alt="heroimg"
+                className="img-fluid"
               />
-              <div className="input-group-text">Get it now</div>
+              <img
+                src="./images/hero4.jpg"
+                alt="heroimg4"
+                className="img-fluid main-hero-img2"
+              />
             </div>
           </div>
-          {/* main header right side */}
-          <div className="col-12 col-lg-6 header-right-side d-flex justify-content-center align-item-center">
-            <img
-              src="../images/hero1.jpg"
-              alt="heroimg"
-              className="img-fluid"
-            />
-            <img
-              src="../images/hero4.jpg"
-              alt="heroimg4"
-              className="img-fluid"
-            />
-          </div>
-        </div>
-      </section>
-    </header>
+        </section>
+      </header>
+    </>
   );
 };
 
