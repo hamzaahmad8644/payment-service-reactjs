5885f227c22d58654a7872b19603854e7055ffc0
Hamza Ahmad <hamzaahmad8644@gmail.com>
2021-10-05 04:27:38 +0500

    Design Complete

--- src/App.js
+++ src/App.js
@@ -2,13 +2,20 @@ import React from "react";
 import Navbar from "./navbar";
 import Header from "./Header";
 import HowitWorks from "./HowitWorks";
-
+import Aboutus from "./Aboutus";
+import Services from "./Services";
+import Contact from "./Contact";
+import Footer from "./Footer";
 const App = () => {
   return (
     <>
       <Navbar />
       <Header />
       <HowitWorks />
+      <Aboutus />
+      <Services />
+      <Contact />
+      <Footer />
     </>
   );
 };
--- src/HowitWorks.js
+++ src/HowitWorks.js
@@ -1,32 +1,35 @@
-import React from "react";
-import { useState } from "react/cjs/react.development";
+import React, { useState } from "react";
 import workapi from "./API/workApi.js";
 
-const HowitWorks = () => {
+const HowItWorks = () => {
   const [workData, setWorkData] = useState(workapi);
   console.log(workData);
   return (
     <>
       <section>
-        <div className="work-container container"></div>
-        <h1 className="main-heading text-center">How does it Work</h1>
-        <div className="row">
-          {workData.map(curElem => {
-            const { id, logo, title, info } = curElem;
-            return (
-              <>
-                <div className="col-12 col-lg-4 text-center work-container subdiv">
-                  <i class={`fontawesome-style ${logo}`}></i>
-                  <h2 className="sub-heading">{title}</h2>
-                  <p className="main-hero-para w-100">{info}</p>
-                </div>
-              </>
-            );
-          })}
+        <div className="work-container container">
+          <h1 className="main-heading text-center">How does it Work</h1>
+          <div className="row">
+            {workData.map(curElem => {
+              const { id, logo, title, info } = curElem;
+              return (
+                <>
+                  <div
+                    className="col-12 col-lg-4 text-center work-container-subdiv"
+                    key={id}
+                  >
+                    <i class={`fontawesome-style ${logo}`}></i>
+                    <h2 className="sub-heading">{title}</h2>
+                    <p className="main-hero-para w-100">{info}</p>
+                  </div>
+                </>
+              );
+            })}
+          </div>
         </div>
       </section>
     </>
   );
 };
 
-export default HowitWorks;
+export default HowItWorks;
